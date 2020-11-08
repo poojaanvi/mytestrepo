44d7607075cf03805d45cba2456470d181927746
poojaanvi <poojaanvi655@gmail.com>
2020-11-08 14:34:30 +0530

    platform1added

--- sketch.js
+++ sketch.js
@@ -19,6 +19,7 @@ function setup(){
     
     ground = new Ground(600,height,1200,20)
     platform =new Ground(200,300,400,20)
+    platform1 = new Ground( 300,200,400,20)
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -49,6 +50,7 @@ function draw(){
     box2.display();
     ground.display();
     platform.display();
+    platform1.display();
     pig1.display();
     log1.display();
 
